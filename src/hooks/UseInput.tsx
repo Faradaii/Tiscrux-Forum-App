568bebe60b4460862e3aca244208582166394cd3
Faradaii B <kdkfrdy@gmail.com>
2024-03-08 21:36:39 +0800

    init: hooks for inputs

---
+++ src/hooks/UseInput.tsx
@@ -0,0 +1,25 @@
+import { useState, type ChangeEvent } from 'react';
+
+type InputHandler = (e: ChangeEvent<HTMLInputElement>) => void;
+type DivHandler = (e: ChangeEvent<HTMLDivElement>) => void;
+
+function useInput (typeInput: number = 0, defaultValue: string = ''): [string, InputHandler | DivHandler, React.Dispatch<React.SetStateAction<string>>] {
+  const [value, setValue] = useState<string>(defaultValue);
+
+  const onInputHandler: InputHandler = (e) => {
+    setValue(e.target.value);
+  };
+
+  const onInputDivHandler: DivHandler = (e) => {
+    setValue(e.target.innerText);
+  };
+
+  switch (typeInput) {
+    case 1:
+      return [value, onInputDivHandler, setValue];
+    default:
+      return [value, onInputHandler, setValue];
+  }
+}
+
+export default useInput;
