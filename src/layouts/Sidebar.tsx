55941ffaa45b4998a9d6352bca67804447f9d7cc
Faradaii B <kdkfrdy@gmail.com>
2024-03-08 21:56:57 +0800

    update: update sidebar component

--- src/layouts/Sidebar.tsx
+++ src/layouts/Sidebar.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import Image from 'next/image'
-import type { User } from '../../types'
+import React from 'react';
+import Image from 'next/image';
+import type { User } from '../../types';
+import ButtonLink from '../components/buttons/ButtonLink';
 
 function Sidebar ({
   user = null,
@@ -10,37 +11,36 @@ function Sidebar ({
   onSignOut: () => void
 }): React.JSX.Element {
   return (
-    <div>
-      <div className="h-screen overflow-auto flex items-center gap-2 bg-white-light dark:bg-white-dark text-black-light dark:text-black-dark">
-        <aside className="sticky p-3 top-0 z-40 h-full md:w-1/4 border flex flex-col bg-white-light dark:bg-white-dark">
-          <div>
-            <h1 className="font-semibold text-2xl">Tiscrux!</h1>
-          </div>
-          <div className="grow">
-          </div>
-          {
-            (user != null)
-              ? (
-                <div className="flex gap-3 mx-2 relative">
-                  <div>
-                    <Image src={user.avatar} alt="" className="rounded-full w-12" />
-                  </div>
-                  <div className="grow ps-1 flex flex-col">
-                    <small className="font-semibold text-lg">{user.name}</small>
-                    <small className="text-xs">
-                      @
-                      {user.id}
-                    </small>
-                  </div>
-                  <button type="button" onClick={onSignOut}>...</button>
-                </div>
-                )
-              : ''
-          }
-        </aside>
+    <aside className="sticky p-3 top-0 z-40 h-screen md:w-1/4 border flex flex-col bg-white-light dark:bg-white-dark">
+      <div>
+        <h1 className="font-semibold text-2xl">Tiscrux!</h1>
       </div>
-    </div>
-  )
+      <div className="grow">
+        <ButtonLink action="Beranda" />
+        <ButtonLink action="Leaderboard" />
+        <ButtonLink action="Pengaturan" />
+      </div>
+      {
+        (user != null)
+          ? (
+            <div className="flex gap-3 mx-2 relative">
+              <div>
+                <Image src={user.avatar} alt="" className="rounded-full w-12" />
+              </div>
+              <div className="grow ps-1 flex flex-col">
+                <small className="font-semibold text-lg">{user.name}</small>
+                <small className="text-xs">
+                  @
+                  {user.id}
+                </small>
+              </div>
+              <button type="button" onClick={onSignOut}>...</button>
+            </div>
+            )
+          : ''
+      }
+    </aside>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
