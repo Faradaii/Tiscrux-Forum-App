dc24f3472c5fe4f46263aba1cd5f8c042de15d7c
Faradaii B <kdkfrdy@gmail.com>
2024-03-08 23:41:32 +0800

    update: updating types

--- types/index.ts
+++ types/index.ts
@@ -1,1 +1,63 @@
-export * from './user';
+export interface User {
+  id: string
+  name: string
+  email: string
+  avatar: string
+}
+
+export interface Token {
+  name: string
+  email: string
+  password: string
+}
+
+export interface AllUser {
+  users: User[]
+}
+
+interface ThreadBase {
+  id: string
+  title: string
+  body: string
+  category: string
+  createdAt: string
+  ownerId: string
+  upVotesBy: string[]
+  downVotesBy: string[]
+}
+
+export interface Threads extends ThreadBase {
+  totalComments: number
+}
+
+export interface Comment {
+  id: string
+  content: string
+  createdAt: string
+  upVotesBy: string[]
+  downVotesBy: string[]
+  owner: Omit<User, 'email'>
+}
+
+export interface ThreadDetail extends ThreadBase {
+  owner: Omit<User, 'email'>
+  comments: Comment[]
+}
+
+export interface LeaderboardData extends User {
+  score: number
+}
+
+interface Vote {
+  id: string
+  userId: string
+  voteType: number
+}
+
+export interface VoteThread extends Vote {
+  threadId: string
+}
+
+export interface VoteComment extends Vote {
+  commentId: string
+}
