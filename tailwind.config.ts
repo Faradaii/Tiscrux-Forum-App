d1be700e9df56793722a542d5be90b1457942e57
Faradaii B <kdkfrdy@gmail.com>
2024-03-09 21:26:53 +0800

    update: update config and rules

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,63 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
+  darkMode: 'class',
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/layouts/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    fontFamily: {
+      poppins: ['poppins', 'sans-serif'],
+      geologica: ['geologica', 'sans-serif']
     },
+    extend: {
+      colors: {
+        white: {
+          light: '#f7f7f7',
+          DEFAULT: '#ffffff',
+          dark: '#191919'
+        },
+        black: {
+          light: '#000000',
+          DEFAULT: '#000000',
+          dark: '#f1f1f1'
+        },
+        primary: '#00C8AC',
+        secondary: '#039D8C',
+        'lime-1': {
+          light: '#A0E4CB',
+          DEFAULT: '#A0E4CB',
+          dark: '#13694A'
+        },
+        'lime-2': {
+          light: '#00ADB5',
+          DEFAULT: '#A0E4CB',
+          dark: '#13694A'
+        },
+        'red-1': '#FF6359',
+        'gray-1': '#8C8C8C',
+        'dark-1': {
+          light: '#312C53',
+          DEFAULT: '#312C53',
+          dark: '#f1f1f1'
+        },
+        'dark-2': {
+          light: '#262626',
+          DEFAULT: '#1f1f1f',
+          dark: '#101010'
+        },
+        'dark-3': {
+          light: '#393e46',
+          DEFAULT: '#1f1f1f',
+          dark: '#101010'
+        }
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
+
 export default config;
