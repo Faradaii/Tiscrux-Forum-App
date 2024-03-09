3ae100b0a2d273e5cc28606dfa86ce1924396b22
Faradaii B <kdkfrdy@gmail.com>
2024-03-09 23:54:15 +0800

    update: updating and refactor

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app';
+import React from 'react';
+import '@/styles/globals.css';
+import { Provider } from 'react-redux';
+import store from '../store/store';
+import Layout from '../layouts';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App ({ Component, pageProps }: AppProps):
+JSX.Element {
+  return (
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
+  );
 }
