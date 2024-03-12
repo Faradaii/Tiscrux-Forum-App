622b97e81e748cdfad67457edda548a1b9c955bc
Faradaii B <kdkfrdy@gmail.com>
2024-03-12 21:14:06 +0800

    update: updating and duplicate skeleton

--- src/components/skeleton/SkeletonThreadCategory.tsx
+++ src/components/skeleton/SkeletonThreadCategory.tsx
@@ -1,11 +1,19 @@
 function SkeletonThreadCategory (): JSX.Element {
   return (
-    <div className="flex w-5/6 m-auto rounded-md animate-pulse px-2">
-      <div className="my-3 grow flex flex-col gap-1">
-        <div className="min-h-4 max-w-20 dark:bg-dark-2-light bg-gray-200 rounded-sm" />
-        <div className="min-h-3 max-w-10 dark:bg-dark-2-light bg-gray-200 rounded-sm" />
+    <>
+      <div className="flex w-5/6 m-auto rounded-md animate-pulse px-2">
+        <div className="my-3 grow flex flex-col gap-1">
+          <div className="min-h-4 max-w-20 dark:bg-dark-2-light bg-gray-200 rounded-sm" />
+          <div className="min-h-3 max-w-10 dark:bg-dark-2-light bg-gray-200 rounded-sm" />
+        </div>
       </div>
-    </div>
+      <div className="flex w-5/6 m-auto rounded-md animate-pulse px-2">
+        <div className="my-3 grow flex flex-col gap-1">
+          <div className="min-h-4 max-w-20 dark:bg-dark-2-light bg-gray-200 rounded-sm" />
+          <div className="min-h-3 max-w-10 dark:bg-dark-2-light bg-gray-200 rounded-sm" />
+        </div>
+      </div>
+    </>
   );
 }
 
