bd25a7cbb6a06d2f5f4631df87ba14a8d7e0c7e9
Faradaii B <kdkfrdy@gmail.com>
2024-03-14 22:35:21 +0800

    init: init theme context

--- src/context/ThemeContext.ts
+++ src/context/ThemeContext.ts
@@ -1,8 +1,12 @@
 import { createContext } from 'react';
 import { getDefaultBrowserTheme } from '@/utils';
+import { type ThemeType } from '../../types';
 
-const defaultTheme = getDefaultBrowserTheme();
+const defaultTheme: ThemeType = {
+  theme: getDefaultBrowserTheme(),
+  toggleTheme: () => {}
+};
 
-const ThemeContext = createContext(defaultTheme);
+const ThemeContext = createContext<ThemeType>(defaultTheme);
 
 export default ThemeContext;
