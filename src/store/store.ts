ed405003fe3c68b57ceb4adc599efc620848b80a
Faradaii B <kdkfrdy@gmail.com>
2024-03-14 22:40:23 +0800

    update: remove unused const

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { loadingBarMiddleware } from 'react-redux-loading-bar';
-import { useDispatch } from 'react-redux';
 import { rootReducer } from './rootReducer';
 
 const store = configureStore({
@@ -13,6 +12,5 @@ const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-export const useAppDispatch: () => AppDispatch = useDispatch;
 
 export default store;
